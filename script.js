5825106a6d58a825c4fe64991c93a3582c537d7a
Gianpaolo Del Moro <giampaolo.delmoro99@gmail.com>
2026-06-12 17:50:51 +0200

    Primo commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const btnNo = document.getElementById('btn-no');
+const btnYes = document.getElementById('btn-yes');
+const gif = document.getElementById('gif');
+const question = document.getElementById('question');
+const button = document.getElementById('btn-yes');
+
+function scappaNo() {
+    const x = Math.random() * (window.innerWidth - btnNo.offsetWidth);
+    const y = Math.random() * (window.innerHeight - btnNo.offsetHeight);
+
+    btnNo.style.position = 'fixed';
+    btnNo.style.left = x + 'px';
+    btnNo.style.top = y + 'px';
+}
+
+btnNo.addEventListener('mouseover', scappaNo);
+btnNo.addEventListener('click', scappaNo);
+btnNo.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    scappaNo();
+});
+
+btnYes.addEventListener('click', () => {
+    question.innerHTML = "Sapevo che avresti detto di sì! 🥰 Anche secondo me sei un gran Fi..sica ❤️";
+    
+    
+    gif.src = "asset/vittoria.gif";
+    
+    btnNo.style.display = 'none';
+    btnYes.style.transform = 'scale(1.3)';
+    btnYes.disabled = true;
+
+    button.textContent = "Grazie per il tuo tempo 🥹🥹🥹"
+});
